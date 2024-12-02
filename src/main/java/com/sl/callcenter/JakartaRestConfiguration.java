8b34b2e8f7968fba83aae089914a6e0075437eaf
Silvio <silvio.limeria@live.com>
2024-12-02 19:10:19 -0300

    change name

--- src/main/java/com/sl/callcenter/JakartaRestConfiguration.java
+++ src/main/java/com/sl/callcenter/JakartaRestConfiguration.java
@@ -1,4 +1,4 @@
-package com.sl.hello;
+package com.sl.callcenter;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
--- src/main/java/com/sl/callcenter/resources/JakartaEE11Resource.java
+++ src/main/java/com/sl/callcenter/resources/JakartaEE11Resource.java
@@ -1,4 +1,4 @@
-package com.sl.hello.resources;
+package com.sl.callcenter.resources;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
